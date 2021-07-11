443253515e38d07bdfb02b046cb91a85fe1dc515
EvelyneMakar <evelyne.makar98@e-uvt.ro>
2021-07-12 00:22:00 +0300

    Second commit

---
+++ Java Exception Handling/src/main/java/MyCalculator.java
@@ -0,0 +1,17 @@
+public class MyCalculator {
+    /*
+     * Create the method long power(int, int) here.
+     */
+    public long power (int n, int p) throws Exception {
+        if (n <0 || p < 0)
+            throw new Exception("n or p should not be negative.");
+        else if (n == 0 && p == 0 ){
+            throw new Exception("n and p should not be zero.");
+        }
+        else {
+            int pow=1;
+            for (int i = p; p>0; p--)
+                pow*=n;
+            return pow;}
+    }
+}
